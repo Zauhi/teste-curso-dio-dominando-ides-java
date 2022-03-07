7ceebc54bb031626351aa60a2653737a3c00ef63
Zauhi <jjvitor7@gmail.com>
2022-03-07 09:59:07 -0300

    adjusted

--- src/com/dio/calculadora/Calculadora.java
+++ src/com/dio/calculadora/Calculadora.java
@@ -6,19 +6,22 @@ public class Calculadora {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-				
+		
 		int a, b;
+		
 		System.out.println("Digite o primeiro valor: ");
 		a = scan.nextInt(); 
 		System.out.println("Digite o segundo valor: ");
 		b = scan.nextInt(); 
-}	
-		int soma = soma(a,b);
-		int subtracao = subtracao(a,b);
+		
+		
+		int soma = soma(a, b);
+		int subtracao = subtracao(a, b);
 		int multiplicacao = multiplicacao(a,b);
 		int divisao = divisao(a,b);
-		
-		System.out.println(soma);
+			
+			
+	    System.out.println(soma);
 		System.out.println(subtracao);
 		System.out.println(multiplicacao);
 		System.out.println(divisao);
